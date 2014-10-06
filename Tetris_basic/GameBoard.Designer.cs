b32402d194b9c682f07aefc4ba387fe0369bf833
rahul <rahuldh1@yahoo.com>
2014-10-06 14:32:44 +0530

    Disabled resizing of window for user

--- Tetris_basic/GameBoard.Designer.cs
+++ Tetris_basic/GameBoard.Designer.cs
@@ -100,6 +100,7 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameBoard";
             this.Text = "Tetris";
