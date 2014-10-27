9b1bb6c3f8756b49448b59b6eb9c5b0041969b8b
rahuldh1 <rahuldh1@yahoo.com>
2014-10-27 16:32:03 +0530

    added gamecontroller interface

---
+++ Tetris_basic/IGameController.cs
@@ -0,0 +1,29 @@
+﻿/*
+Copyright 2014 Rahul Dharmadhikari
+
+This file is part of Tetris_basic.
+
+Tetris_basic is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Tetris_basic is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Tetris_basic. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+
+namespace Tetris_basic
+{
+    public abstract class IGameController
+    {
+        public abstract void OnKeyDown(object sender, KeyEventArgs e);
+    }
+}
